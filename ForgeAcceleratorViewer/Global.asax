e5e02c5a734cf6554098268883bdea59e56f9b0f
João Paulo <jpornelas@poli.ufrj.br>
2020-04-23 10:01:54 -0300

    first commit

---
+++ ForgeAcceleratorViewer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ForgeAcceleratorViewer.WebApiApplication" Language="C#" %>
---
+++ ForgeAcceleratorViewer/js/ForgeViewer.js
@@ -0,0 +1,54 @@
+﻿var viewer;
+
+function launchViewer(urn) {
+    var options = {
+        env: 'AutodeskProduction',
+        getAccessToken: getForgeToken
+    };
+
+    Autodesk.Viewing.Initializer(options, () => {
+        viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer'), { extensions: ['Autodesk.DocumentBrowser'] });
+        viewer.start();
+        viewer.loadExtension('IconMarkupExtension', {
+            button: {
+                icon: 'fa-thermometer-half',
+                tooltip: 'Show Temperature'
+            },
+            icons: [
+                { dbId: 3944, label: '300&#176;C', css: 'fas fa-thermometer-full' },
+                { dbId: 721, label: '356&#176;C', css: 'fas fa-thermometer-full' },
+                { dbId: 10312, label: '450&#176;C', css: 'fas fa-thermometer-empty' },
+                { dbId: 563, css: 'fas fa-exclamation-triangle' },
+            ],
+            onClick: (id) => {
+                viewers.select(id);
+                viewers.utilities.fitToView();
+                switch (id) {
+                    case 563:
+                        alert('Sensor offline');
+                }
+            }
+        })
+        var documentId = 'urn:' + urn;
+        Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
+    });
+}
+
+function onDocumentLoadSuccess(doc) {
+    var viewables = doc.getRoot().getDefaultGeometry();
+    viewer.loadDocumentNode(doc, viewables).then(i => {
+        // documented loaded, any action?
+    });
+}
+
+function onDocumentLoadFailure(viewerErrorCode) {
+    console.error('onDocumentLoadFailure() - errorCode:' + viewerErrorCode);
+}
+
+function getForgeToken(callback) {
+    fetch('/api/forge/oauth/token').then(res => {
+        res.json().then(data => {
+            callback(data.access_token, data.expires_in);
+        });
+    });
+}
